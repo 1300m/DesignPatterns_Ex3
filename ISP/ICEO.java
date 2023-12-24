623ac238be3a0f1e3e2c804ffe005f76096cf34a
Jirapat Pichai <113757195+1300m@users.noreply.github.com>
2023-12-24 13:21:11 +0700

    Update ICEO.java

--- ISP/ICEO.java
+++ ISP/ICEO.java
@@ -1,7 +1,6 @@
 public interface ICEO extends IEmployee {
-    //for ceo
-	public void makeDecisions();	
-	public void addStocks();
+    public void makeDecisions();	
+    public void addStocks();
 
     public void addBonus();
 }
